f1ffb8cb623401434279845995936d403cd2b5fe
arrovain <arrovain@gmail.com>
2024-03-09 18:06:53 +0300

    first commit

---
+++ tailwind.config.ts
@@ -0,0 +1,31 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        mont: ['var(--font-inter)'],
+        mono: ['var(--font-roboto-mono)'],
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+          dark: "#1b1b1b",
+          light: "#f5f5f5",
+          primary: "#B63E96", // 240,86,199
+          primaryDark: "#58E6D9", // 80,230,217
+      }
+    
+    },
+  },
+  plugins: [],
+};
+export default config;
